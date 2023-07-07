96d3ba635987922018d6f513b739299ff9ebe3f8
Alan Barrera <aalan.eduardo@gmail.com>
2023-07-06 23:21:08 -0600

    archivo de barril

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,7 @@
 
+import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 
 class HomeScreen extends StatelessWidget {
@@ -12,7 +14,46 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
 
-        body: Placeholder()
+        body: _HomeView()
+    );
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read( nowPlayingMoviesProvider.notifier ).loadNextPage();
+    
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );
+
+    // if( nowPlayingMovies.isEmpty ) return const CircularProgressIndicator();
+
+    return ListView.builder(
+      itemCount: nowPlayingMovies.length,
+      itemBuilder: ( context, index ){
+
+        final movie = nowPlayingMovies[ index ];
+
+        return ListTile(
+          title: Text( movie.title ),
+        );
+
+      }
     );
   }
 }
